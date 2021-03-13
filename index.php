b3c8924d587ca68579fa4fb7e36d619b407fdda5
ah-rifat <rifat266@gmail.com>
2021-03-13 20:16:15 +0600

    create home page

--- index.php
+++ index.php
@@ -3,17 +3,107 @@
 <main class="content">
 	<div class="container-fluid p-0">
 
-		<h1 class="h3 mb-3">Blank Page</h1>
+		<h1 class="h3 mb-3">Student Home Page</h1>
 
 		<div class="row">
-			<div class="col-12">
+			<div class="col-6">
 				<div class="card">
-					<div class="card-header">
-						<h5 class="card-title mb-0">Home Empty card</h5>
+					<div class="card-header" style="background-color: #343a40;">
+						<h5 class="card-title mb-0 text-light" style="font-size: medium;">My Information</h5>
 					</div>
 					<div class="card-body">
+						<div class="table-responsive-sm">
+							<table class="table">
+								<tr>
+									<th>Image:</th>
+									<td class="text-center p-4"><img src="img/avatars/avatar-4.jpg" /></td>
+								</tr>
+								<tr>
+									<th>Name:</th>
+									<td>Md. Kasam</td>
+								</tr>
+								<tr>
+									<th>Email:</th>
+									<td>example@gmail.com</td>
+								</tr>
+								<tr>
+									<th>Phone:</th>
+									<td>01524856985</td>
+								</tr>
+								<tr>
+									<th>Address:</th>
+									<td>Lorem ipsum dolor sit amet consectetur adipisicing
+										elit. Non recusandae eum sint maxime expedita.</td>
+								</tr>
+								<tr>
+									<th>class</th>
+									<td>05</td>
+								</tr>
+								<tr>
+									<th>section</th>
+									<td>A</td>
+								</tr>
+								<tr>
+									<th>Roll</th>
+									<td>20200511</td>
+								</tr>
+							</table>
+						</div>
 					</div>
 				</div>
+			</div>
+			<div class="col-6">
+				<div class="row">
+					<div class="col-12">
+						<div class="card">
+							<div class="card-header" style="background-color: #343a40;">
+								<h5 class="card-title mb-0 text-light" style="font-size: medium;">Notice Board</h5>
+							</div>
+							<div class="card-body" style=" margin-left: 8px;">
+								<div class="row p-1">31 May, 2020</div>
+								<div class="row p-1 font-weight-bold text-info">Title</div>
+								<div class="row p-1">Discription-gewewrerfewgtrfshgsretresgfdgrestresgsredytgret</div>
+								<hr>
+								<div class="row p-1">31 May, 2020</div>
+								<div class="row p-1 font-weight-bold text-info">Title</div>
+								<div class="row p-1">Discription-gewewrerfewgtrfshgsretresgfdgrestresgsredytgret</div>
+								<hr>
+								<div class="row p-1">31 May, 2020</div>
+								<div class="row p-1 font-weight-bold text-info">Title</div>
+								<div class="row p-1">Discription-gewewrerfewgtrfshgsretresgfdgrestresgsredytgret</div>
+								<hr>
+								<div class="row p-1">31 May, 2020</div>
+								<div class="row p-1 font-weight-bold text-info">Title</div>
+								<div class="row p-1">Discription-gewewrerfewgtrfshgsretresgfdgrestresgsredytgret</div>
+								<hr>
+								<nav aria-label="Page navigation example" class="mt-5">
+									<ul class="pagination justify-content-center">
+										<li class="page-item disabled">
+											<a class="page-link" href="#" tabindex="-1">Previous</a>
+										</li>
+										<li class="page-item"><a class="page-link" href="#">1</a></li>
+										<li class="page-item"><a class="page-link" href="#">2</a></li>
+										<li class="page-item"><a class="page-link" href="#">3</a></li>
+										<li class="page-item">
+											<a class="page-link" href="#">Next</a>
+										</li>
+									</ul>
+								</nav>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- <div class="row">
+					<div class="col-12">
+						<div class="card">
+							<div class="card-header">
+								<h5 class="card-title mb-0">Academic Calander</h5>
+							</div>
+							<div class="card-body">
+							</div>
+						</div>
+					</div>
+				</div> -->
 			</div>
 		</div>
 
